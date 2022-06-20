bad0f74762a4953ffd69c5213c3fa8abde98edfc
Ruslan <ri4i23aminov@yandex.ru>
2022-06-20 15:39:00 +0300

    osnovnaya chast

--- kontrrab/Program.cs
+++ kontrrab/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿string[] array = new string[] { "one", "-2", "hello", "on", "second" };
+string[] newarray = new string[array.Length];
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newarray[count] = array[i];
+            count++;
+        } 
+    }
+}
+
+SecondArray(array, newarray);
+
+
